d6cffb0d11829e9355866303fc29a5853ab3751b
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-20 13:33:58 -0700

    [ADD] Empezando el proyecto utilizando css modules

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styles from './App.module.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className={styles.title}>buscador de clima</h1>
+
+      <div className={styles.container}>
+
+      </div>
+    </>
+  )
+}
+
+export default App
